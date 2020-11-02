b2776caf0d21581c3e6dab8196afe47e42f48ee4
OlegSvrk <serv_all@mail.ru>
2020-11-02 20:23:01 +0300

    Добавлен ввод сервер:порт при запуске

--- IPtoMail/Program.cs
+++ IPtoMail/Program.cs
@@ -37,7 +37,7 @@ namespace IPtoMail
         }
 
         static void SendMessage(string mailUserName, string mailPassword, string recipientAddress, string body, out bool sendingOK)
-        {//TODO ?вместо метода ввести класс, передавать имя-пароль в конструкторе?
+        {
             var sender = new MailAddress(mailUserName);
             var recipient = new MailAddress(recipientAddress);
 
@@ -47,7 +47,7 @@ namespace IPtoMail
                 Body = body
             };
 
-            using (var client = new SmtpClient("smtp.mail.ru", 25))
+            using (var client = new SmtpClient(smtpServer, smtpPort))
             {
                 client.Credentials = new NetworkCredential(mailUserName, mailPassword);
                 client.EnableSsl = true;
@@ -56,10 +56,16 @@ namespace IPtoMail
                 {
                     client.Send(message);
                     sendingOK = true;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"{DateTime.Now} {recipientAddress} sending ok");
+                    Console.ResetColor();
                 }
                 catch (Exception)
                 {
                     sendingOK = false;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"{DateTime.Now} {recipientAddress} sending false");
+                    Console.ResetColor();
                 }
 
             };
@@ -67,10 +73,12 @@ namespace IPtoMail
 
         static void RecipientsListFormer()
         {
+Console.WriteLine("recipient list re-formed");
             recipientsList.Clear();
             recipientsList.Add(mailsenderUserName);
             string[] rec;
             string[] recipients = File.ReadAllLines("recipients.list");
+
             for (int i = 0; i < recipients.Length; i++)
             {
                 if (recipients[i].StartsWith("#")) continue;
@@ -79,55 +87,82 @@ namespace IPtoMail
                 recipientsList.Add(rec[0]);
             }
         }
+        
+        #region VARIABLES
+            static List<string> recipientsList = new List<string>();
+            static DateTime lastTimeRecipientsListChanged;
+            static string mailsenderUserName = "",
+                          smtpServer = "smtp.mail.ru";
+            static ushort smtpPort = 25;
 
-        static List<string> recipientsList = new List<string>();
-        static DateTime lastTimeRecipientsListChanged;
-        static string mailsenderUserName = "";
+            const string recipientsFile = "recipients.list",
+                         logFile = "events.log";
 
-        const string recipientsFile = "recipients.list",
-                     logFile = "events.log";
+        #endregion
         static void Main(string[] args)//args: mailUserName
         {
-            if (args.Length != 1)
+            if (args.Length != 2)
             {
-                Console.WriteLine("The arguments is: mailUserName");
+                Console.WriteLine("The arguments is: mailUserName server:port\ne.g. user@server.com smtp.server.com:25");
             }
             else
             {
                 mailsenderUserName = args[0];
-                CheckingFiles();
-                Console.WriteLine("Enter mailPassword:");
-                string mailPassword = Console.ReadLine();
-                Console.Clear();
-                string
-                    currentIP = "",
-                    mbNewIP;
-                while (true)
+                if (TryParseArgs(args[1]))
                 {
-                    CheckRecipientsListUpdate();
-                    mbNewIP = GetIP(out bool IPaddressOK);
-                    if (currentIP != mbNewIP && IPaddressOK)
+                    CheckingFiles();
+                    Console.WriteLine("Enter mailPassword:");
+                    string mailPassword = Console.ReadLine();
+                    Console.Clear();
+                    string
+                        currentIP = "",
+                        mbNewIP;
+                    while (true)
                     {
-                        currentIP = mbNewIP;
-                        Console.WriteLine($"{DateTime.Now} your IP is {currentIP}");
-                        //CheckRecipientsListUpdate();
-
-                        foreach (string recipient in recipientsList)
+                        mbNewIP = GetIP(out bool IPaddressOK);
+                        if (currentIP != mbNewIP && IPaddressOK)
                         {
-                            SendMessage(mailsenderUserName, mailPassword, recipient, currentIP, out bool sendingOK);
-                            if (!sendingOK)
-                            {
-                                Console.ForegroundColor = ConsoleColor.Red;
-                                Console.WriteLine($"{DateTime.Now} error sending e-mail. Check username, password and connection");
-                                Console.ResetColor();
-                            }
+                            currentIP = mbNewIP;
+                            Console.WriteLine($"{DateTime.Now} your IP is {currentIP}");
+                            CheckRecipientsListUpdate();
 
+                            foreach (string recipient in recipientsList)
+                            {
+                                SendMessage(mailsenderUserName, mailPassword, recipient, currentIP, out bool sendingOK);
+                                if (!sendingOK)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine($"{DateTime.Now} error sending e-mail. Check username, password and connection");
+                                    Console.ResetColor();
+                                }
+
+                            }
                         }
+                        Thread.Sleep(60000);
                     }
-                    Thread.Sleep(60000);
+
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input parameters");
                 }
             }
 
+        }
+
+        private static bool TryParseArgs(string v)
+        {
+            if (v.Contains(":"))
+            {
+                string[] str = v.Split(':');
+                smtpServer = str[0];
+
+                if (ushort.TryParse(str[1], out smtpPort))
+                    return true;
+
+            }
+            
+            return false;
         }
 
         private static void CheckRecipientsListUpdate()
@@ -141,21 +176,27 @@ namespace IPtoMail
 
         static void CheckingFiles()
         {
-            if (!File.Exists(recipientsFile))
-            {
-                using(File.Create(recipientsFile))
-                
-                lastTimeRecipientsListChanged = File.GetLastWriteTime(recipientsFile);
-            }
-            else CheckRecipientsListUpdate();
-
             if (!File.Exists(logFile))
             {
                 using (File.Create(logFile));
-
             }
 
-            //lastTimeRecipientsListChanged = File.GetLastWriteTime(recipientsFile);
+
+
+            if (!File.Exists(recipientsFile))
+            {
+                using (File.Create(recipientsFile));
+                
+            }
+            else
+            {
+                RecipientsListFormer();
+            }
+            //CheckRecipientsListUpdate();
+            lastTimeRecipientsListChanged = File.GetLastWriteTime(recipientsFile);
+            
+
+
         }
     }
 }
