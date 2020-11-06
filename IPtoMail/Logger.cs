1230c8ba7d7798dc0123079b18561a8c0499c5ae
OlegSvrk <serv_all@mail.ru>
2020-11-06 17:23:31 +0300

    Logger буферизует очередь событий в случае невозможности записать в файл, после восстановления доступа к файлу все события из буфера записываются

--- IPtoMail/Logger.cs
+++ IPtoMail/Logger.cs
@@ -9,9 +9,11 @@ namespace IPtoMail
 {
     static class Logger
     {
+        static List<string> eventStorage = new List<string>();
+        static bool eventStorageNotEmpty;
+
         public static bool WriteLogEvent(List<string> events, ConsoleColor color = ConsoleColor.Gray)
         {
-            //TODO оформить систему логирования как отдельный класс (static), и => дергать приямо из методов
             Console.ForegroundColor = color;
             foreach (var item in events)
             {
@@ -19,19 +21,39 @@ namespace IPtoMail
             }
             Console.ResetColor();
 
+            if (eventStorage.Count != 0)
+                foreach (var item in events)
+                {
+                    eventStorage.Add(item);
+                }
+            else
+            {
+                eventStorage = events;
+            }
+
             try
             {
-                File.AppendAllLines(Program.logFile, events);
-
+                File.AppendAllLines(Program.logFile, eventStorage);
             }
-            catch (IOException)//TODO возможно, отложенная запись на случай ошибки?
+            catch (Exception)//TODO возможно, отложенная запись на случай ошибки?
             {
+
+                eventStorageNotEmpty = true;
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.BackgroundColor = ConsoleColor.DarkBlue;
-                Console.WriteLine($"< < < < < \t{DateTime.Now}: Cant write event to log\t > > > > >");
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"< < < < < \t{DateTime.Now}: Cant write events to log, events buffering\t > > > > >");
                 Console.ResetColor();
                 return false;
             }
+            
+            if (eventStorageNotEmpty)
+            {
+                eventStorageNotEmpty = false;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"{DateTime.Now} Event log accessible, events queue wrote to log successful");
+                Console.ResetColor();
+            }
+            eventStorage.Clear(); //если запись в файл успешна
             return true;
 
         }
